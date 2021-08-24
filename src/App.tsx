722052fc2422e70bc5d52d02c2cbaa678501d78b
naoto hamada <hamada@vis-its.com>
2021-08-24 12:32:28 +0900

    Add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        < a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -18,7 +15,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div >
   );
 }
 
