4a9c288f6d0078864748b5f21928cf3b3a7d840e
naoto hamada <hamada@vis-its.com>
2021-08-25 09:41:05 +0900

    Add Table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
+import Table from "./Table";
+
 function App() {
   return (
     <div>
-      <header className="App-header">
-        < a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table />
     </div >
   );
 }
---
+++ src/Table.tsx
@@ -0,0 +1,5 @@
+export default function Table({ hoge = 'hoge2' }: { hoge?: string }) {
+  return (
+    <div>{hoge}</div>
+  );
+}
