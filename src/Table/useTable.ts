ab61a84890cff9f2464300a2862a403d2191cafc
naoto hamada <hamada@vis-its.com>
2021-09-02 01:50:28 +0900

    Fix to use localstorage (#5)
    * Fix to use localstorage
    
    * Update README

--- src/Table/useTable.ts
+++ src/Table/useTable.ts
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useCallback, useState } from "react";
 
 export const PAGINATION_MAX_COUNT = 15;
 export const MAX_FONT_SIZE = 32;
+
+function validateFontSize(fontSize: number) {
+  return Number.isInteger(fontSize) && fontSize > 0 && fontSize <= MAX_FONT_SIZE;
+}
 
 export default function useTable() {
   const [index, setIndex] = useState<number>(1);
@@ -22,13 +26,22 @@ export default function useTable() {
   const handleChangeFontSize = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     const fontSize = parseInt(event.target.value);
 
-    if (!Number.isInteger(fontSize) || fontSize <= 0 || fontSize > MAX_FONT_SIZE) {
+    if (!validateFontSize(fontSize)) {
       setError(`Please set an integer from 1 to ${MAX_FONT_SIZE} on the font-size.`);
       return;
     }
 
     setError('');
     setFontSize(fontSize);
+    localStorage.setItem('fontSize', String(fontSize));
+  }, [setFontSize]);
+
+  useEffect(() => {
+    const savedFontSize = localStorage.getItem('fontSize');
+    if (!savedFontSize || !parseInt(savedFontSize)) return;
+
+    const parseFontSize = parseInt(savedFontSize);
+    if (validateFontSize(parseFontSize)) setFontSize(parseFontSize);
   }, [setFontSize]);
 
   return {
