bf69ec2782260529c3a663da693cd4ce43534807
naoto hamada <hamada@vis-its.com>
2021-09-05 01:47:30 +0900

    Copy to clipboard (#15)
    * Copy to clipboard
    
    * Refactoring

--- src/Table/Items.tsx
+++ src/Table/Items.tsx
@@ -1,6 +1,7 @@
-import { Grid } from '@material-ui/core';
+import { Alert, Grid, Snackbar } from '@material-ui/core';
 import { styled } from '@material-ui/core/styles';
 import { Paper } from '@material-ui/core';
+import useCopy from './useCopy';
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -15,21 +16,33 @@ const PrimaryItem = styled(Item)(({ theme }) => ({
 }));
 
 export default function Table({ index, fontSize }: { index: number, fontSize: number }) {
+  const { handleCopy, openSnackBar, closeSnackBar } = useCopy();
+
   let list = [];
   const startPx = 1 + (index - 1) * 100;
-
   for (let px = startPx; px < startPx + 100; px++) {
     const rem = px / fontSize;
     list.push(
-      <Grid item>
+      <Grid item key={px} onClick={handleCopy}>
         {Number.isInteger(rem) ? <PrimaryItem>{px}px = {rem}rem</PrimaryItem> : <Item>{px}px = {rem}rem</Item>}
       </Grid>
     );
   }
 
   return (
-    <Grid container spacing={1}>
-      {list}
-    </Grid>
+    <>
+      <Grid container spacing={1}>
+        {list}
+      </Grid>
+      <Snackbar
+        open={openSnackBar}
+        autoHideDuration={6000}
+        onClose={closeSnackBar}
+      >
+        <Alert onClose={closeSnackBar} severity="success" sx={{ width: '100%' }}>
+          Copied rem to the clipboard!!
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
