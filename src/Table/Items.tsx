d423d5958dcf9865803f2e8ec33d65ae01794da0
naoto hamada <hamada@vis-its.com>
2021-09-05 01:45:12 +0900

    Refactoring

--- src/Table/Items.tsx
+++ src/Table/Items.tsx
@@ -1,7 +1,7 @@
-import { useCallback, useState } from 'react';
 import { Alert, Grid, Snackbar } from '@material-ui/core';
 import { styled } from '@material-ui/core/styles';
 import { Paper } from '@material-ui/core';
+import useCopy from './useCopy';
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -16,34 +16,14 @@ const PrimaryItem = styled(Item)(({ theme }) => ({
 }));
 
 export default function Table({ index, fontSize }: { index: number, fontSize: number }) {
+  const { handleCopy, openSnackBar, closeSnackBar } = useCopy();
+
   let list = [];
   const startPx = 1 + (index - 1) * 100;
-
-  const [openSnackBar, setOpenSnackBar] = useState(false);
-
-  const copy = useCallback(
-    (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      const text = event.currentTarget.firstElementChild?.textContent;
-      if (!text) return;
-
-      navigator.clipboard.writeText(text.split(' ')[2]).then(e => {
-        setOpenSnackBar(true);
-      });
-    },
-    [setOpenSnackBar],
-  );
-
-  const handleClose = useCallback(
-    () => {
-      setOpenSnackBar(false);
-    },
-    [setOpenSnackBar],
-  )
-
   for (let px = startPx; px < startPx + 100; px++) {
     const rem = px / fontSize;
     list.push(
-      <Grid item key={px} onClick={copy}>
+      <Grid item key={px} onClick={handleCopy}>
         {Number.isInteger(rem) ? <PrimaryItem>{px}px = {rem}rem</PrimaryItem> : <Item>{px}px = {rem}rem</Item>}
       </Grid>
     );
@@ -57,9 +37,9 @@ export default function Table({ index, fontSize }: { index: number, fontSize: nu
       <Snackbar
         open={openSnackBar}
         autoHideDuration={6000}
-        onClose={handleClose}
+        onClose={closeSnackBar}
       >
-        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+        <Alert onClose={closeSnackBar} severity="success" sx={{ width: '100%' }}>
           Copied rem to the clipboard!!
         </Alert>
       </Snackbar>
---
+++ src/Table/useCopy.ts
@@ -0,0 +1,30 @@
+import { useCallback, useState } from "react";
+
+export default function useCopy() {
+  const [openSnackBar, setOpenSnackBar] = useState(false);
+
+  const handleCopy = useCallback(
+    (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      const text = event.currentTarget.firstElementChild?.textContent;
+      if (!text) return;
+
+      navigator.clipboard.writeText(text.split(' ')[2]).then(e => {
+        setOpenSnackBar(true);
+      });
+    },
+    [setOpenSnackBar],
+  );
+
+  const closeSnackBar = useCallback(
+    () => {
+      setOpenSnackBar(false);
+    },
+    [setOpenSnackBar],
+  );
+
+  return {
+    handleCopy,
+    openSnackBar,
+    closeSnackBar,
+  }
+}
