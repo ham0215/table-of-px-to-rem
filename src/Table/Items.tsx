8545eaa343de505bfe8f4b1c561a76656253e25f
naoto hamada <hamada@vis-its.com>
2021-09-05 01:32:31 +0900

    Copy to clipboard

--- src/Table/Items.tsx
+++ src/Table/Items.tsx
@@ -1,4 +1,5 @@
-import { Grid } from '@material-ui/core';
+import { useCallback, useState } from 'react';
+import { Alert, Grid, Snackbar } from '@material-ui/core';
 import { styled } from '@material-ui/core/styles';
 import { Paper } from '@material-ui/core';
 
@@ -18,18 +19,50 @@ export default function Table({ index, fontSize }: { index: number, fontSize: nu
   let list = [];
   const startPx = 1 + (index - 1) * 100;
 
+  const [openSnackBar, setOpenSnackBar] = useState(false);
+
+  const copy = useCallback(
+    (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      const text = event.currentTarget.firstElementChild?.textContent;
+      if (!text) return;
+
+      navigator.clipboard.writeText(text.split(' ')[2]).then(e => {
+        setOpenSnackBar(true);
+      });
+    },
+    [setOpenSnackBar],
+  );
+
+  const handleClose = useCallback(
+    () => {
+      setOpenSnackBar(false);
+    },
+    [setOpenSnackBar],
+  )
+
   for (let px = startPx; px < startPx + 100; px++) {
     const rem = px / fontSize;
     list.push(
-      <Grid item>
+      <Grid item key={px} onClick={copy}>
         {Number.isInteger(rem) ? <PrimaryItem>{px}px = {rem}rem</PrimaryItem> : <Item>{px}px = {rem}rem</Item>}
       </Grid>
     );
   }
 
   return (
-    <Grid container spacing={1}>
-      {list}
-    </Grid>
+    <>
+      <Grid container spacing={1}>
+        {list}
+      </Grid>
+      <Snackbar
+        open={openSnackBar}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          Copied rem to the clipboard!!
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
