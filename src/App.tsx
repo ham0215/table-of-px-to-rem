6003d71ca5dc85b796c5d381eccb970f54266139
naoto hamada <hamada@vis-its.com>
2021-09-06 01:03:15 +0900

    Fix i18n

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import { Container } from '@material-ui/core';
 import Copyright from './Copyright';
 import Table from './Table';
 import Header from './Header';
+import './i18n';
+import { useTranslation } from 'react-i18next';
 
 export default function App() {
+  const { i18n } = useTranslation();
+  i18n.changeLanguage(window.navigator.language);
+
   return (
     <Container maxWidth="xl">
       <Header />
