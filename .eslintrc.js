8483be94f00e5f55e01ff607a320b6f0cc0867cc
naoto hamada <hamada@vis-its.com>
2021-09-06 09:30:52 +0900

    Fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,6 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    'jest/globals': true,
-  },
-  globals: {
-    process: true,
   },
   extends: [
     'eslint:recommended',
@@ -32,23 +28,13 @@ module.exports = {
     },
   },
   rules: {
-    'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'eol-last': ['error', 'always'],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
-    'jest/consistent-test-it': [
-      'error',
-      {
-        fn: 'it',
-        withinDescribe: 'it',
-      },
-    ],
     'jest/expect-expect': 'off',
     'jest/require-top-level-describe': 'error',
     'jest/no-disabled-tests': 'off',
