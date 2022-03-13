07e85f21cb635b155ffa1faede82174b1a86818e
naoto hamada <hamada@vis-its.com>
2022-03-13 23:48:22 +0900

    Fix prettier

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -6,17 +6,13 @@ import { BrowserRouter } from 'react-router-dom';
 const AllTheProviders: FC = ({ children }) => {
   return (
     <ThemeProvider>
-      <BrowserRouter>
-        {children}
-      </BrowserRouter>
+      <BrowserRouter>{children}</BrowserRouter>
     </ThemeProvider>
   );
 };
 
-const customRender = (
-  ui: ReactElement,
-  options?: Omit<RenderOptions, 'wrapper'>,
-) => render(ui, { wrapper: AllTheProviders, ...options });
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
 
 export * from '@testing-library/react';
 export { customRender as render };
