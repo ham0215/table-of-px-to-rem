75647dacad117da2c557c96ac935a7db76b2cea2
naoto hamada <hamada@vis-its.com>
2022-04-02 23:31:19 +0900

    Add clipboard test (#215)
    * Add clipboard test
    
    * Fix prettier

--- src/Top/Table/Items/index.test.tsx
+++ src/Top/Table/Items/index.test.tsx
@@ -1,5 +1,6 @@
 import Items from '.';
 import { render, waitFor } from 'test-utils';
+import userEvent from '@testing-library/user-event';
 
 describe('Items', () => {
   it('fontSizeが未指定の場合', async () => {
@@ -13,6 +14,39 @@ describe('Items', () => {
     const { getByText } = render(<Items page={1} fontSize="32" />);
     await waitFor(() => {
       getByText('32px = 1rem');
+    });
+  });
+});
+
+describe('Clipboard', () => {
+  beforeEach(() => {
+    Object.assign(navigator, {
+      clipboard: {
+        text: '',
+        readText() {
+          return this.text;
+        },
+        writeText(data: string) {
+          this.text = data;
+          return Promise.resolve();
+        },
+      },
+    });
+  });
+
+  afterAll(() => {
+    Object.assign(navigator, { clipboard: undefined });
+  });
+
+  it('Click to copy to clipboard', async () => {
+    const { getByText } = render(<Items page={1} fontSize="" />);
+    await waitFor(() => {
+      getByText('16px = 1rem');
+    });
+
+    userEvent.click(getByText('16px = 1rem'));
+    await waitFor(() => {
+      expect(navigator.clipboard.readText()).toEqual('1rem');
     });
   });
 });
