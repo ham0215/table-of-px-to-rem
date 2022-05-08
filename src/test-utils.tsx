76f8624363ecaf9bf91984216973a98cbfc142cf
naoto hamada <hamada@vis-its.com>
2022-05-09 00:06:53 +0900

    Fix type

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,10 +1,10 @@
-import { FC, ReactElement } from 'react';
+import { ReactElement } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
 import ThemeProvider from './providers/ThemeProvider';
 import { MemoryRouter } from 'react-router-dom';
 import i18n from 'i18n';
 
-const AllTheProviders: FC = ({ children }) => {
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider>
       <MemoryRouter>{children}</MemoryRouter>
